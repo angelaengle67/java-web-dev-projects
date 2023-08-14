3edaacbf3f23d4b8821f4b67ee17e666a9a622a7
Angela Engle <angelaengle67@gmail.com>
2023-08-14 17:49:05 -0500

    2nd Java exercises

---
+++ control-flow-and-collections/exercises/Array2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Array2 {
+
+    public static void main(String[] args) {
+        String phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+        String[] words = phrase.split(" ");
+        System.out.println(Arrays.toString(words));
+        String[] sentences = phrase.split("\\.");
+        System.out.println(Arrays.toString(sentences));
+    }
+}
---
+++ control-flow-and-collections/exercises/Main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* public class Main {
+     public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        arr.add(7);
+        arr.add(8);
+        arr.add(9);
+        arr.add(10);
+        System.out.println(sumEven(arr));
+    }
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("cheese");
+        words.add("milk");
+        words.add("water");
+        words.add("crackers");
+        words.add("eggs");
+        words.add("brownies");
+        words.add("sugar");
+        words.add("flour");
+        words.add("chicken");
+        words.add("banana");
+        System.out.println(hasFive(words));
+    }
+
+    public static ArrayList<String> hasFive(ArrayList<String> words) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word length: ");
+        int numChars = input.nextInt();
+
+        ArrayList<String> fiveLetters = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() == numChars) {
+                fiveLetters.add(word);
+            }
+        }
+        return fiveLetters;
+    }
+
+
+    public static int sumEven(ArrayList<Integer> arr) {
+        int total = 0;
+        for (int integer : arr) {
+            if (integer % 2 == 0) {
+                total += integer;
+            }
+        }
+        return total;
+    }
+}
+*/
+
+
+
