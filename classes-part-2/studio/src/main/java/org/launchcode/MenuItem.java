470e3c356ebf6438f249dbbfd9fff281dce43c12
Angela Engle <angelaengle67@gmail.com>
2023-09-05 11:03:06 -0500

    Studio complete

--- classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
@@ -1,5 +1,7 @@
 package org.launchcode;
 
+import java.util.Objects;
+
 public class MenuItem {
     private double price;
     private String description;
@@ -27,6 +29,26 @@ public class MenuItem {
 
     public void setNew(boolean aNew) {
         isNew = aNew;
+    }
+
+    public String getIsNew() {
+        return (this.isNew ? "New!" : "Favorite");
+        }
+
+    public String toString () {
+        return "Category: " + this.category +" Description: " + this.description + " Price: " + this.price + " " + this.getIsNew();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return Objects.equals(description, menuItem.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description);
     }
 }
 
